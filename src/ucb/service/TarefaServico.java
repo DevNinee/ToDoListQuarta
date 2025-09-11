a368798b48db7093af0783ad5aef2f7962aae24b
Camile Eduarda <165083973+CamileECF@users.noreply.github.com>
2025-09-10 22:22:45 -0300

    Atualização do editar tarefas para o código ficar mais limpo

--- src/ucb/service/TarefaServico.java
+++ src/ucb/service/TarefaServico.java
@@ -56,18 +56,10 @@ public class TarefaServico {
     }
 
     // Método Editar tarefa no Tarefa Serviço
-    public class EditarTarefa {
-
-    private List<Tarefa> tarefas;
-    public EditarTarefa(List<Tarefa> tarefas) {
-        this.tarefas = tarefas;
-    }
-
 public boolean editarTarefa(Long id, String novoTitulo, String novaDescricao) {
     Optional<Tarefa> tarefa = tarefas.stream()
             .filter(t -> t.getId().equals(id))
             .findFirst();
-
     if (tarefa.isPresent()) {
         Tarefa t = tarefa.get();
         t.setTitulo(novoTitulo);
